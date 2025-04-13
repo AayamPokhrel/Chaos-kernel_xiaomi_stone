801fd875c7a44c9449e8c1d6f0f52e9557c670ed
kamikaonashi <thorschi93@gmx.at>
2025-04-13 20:00:37 +0200

    drivers: power: supply: allow fastcharge up to 43c
    drivers: power: supply: allow fastcharge up to 40c
    
    hacky way, but it works
    after hitting 40c, thermal mitigation kicks in and throttles
    before hitting 40c, jeita also throttles, but not that hard

--- drivers/power/supply/pd_policy_manager.c
+++ drivers/power/supply/pd_policy_manager.c
@@ -897,10 +897,7 @@ static int battery_sw_jeita(struct usbpd_pm *pdpm)
                         pdpm->bat_temp,  jeita_curr, pdpm->therm_curr, pdpm->pps_temp_flag, pd_auth, pdpm->bat_cycle, pdpm->batt_auth);
 
     // Thermal charge bypass
-    bool thermal_boost = thermal_boost_allowed();
-    int thermal_threshold = thermal_boost ? 440 : 400;
-    
-    if (pdpm->bat_temp < thermal_threshold) {
+    if (pdpm->bat_temp < 430) {
         pdpm->therm_curr = jeita_curr;
     }
 
